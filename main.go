ca1d34ee19bea1638ba476134b07d04c22db06dd
Ivan Zezyulya <ivan@zoid.cc>
2017-10-16 19:47:03 +0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func dlog(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, args...)
+}
+
+func main() {
+	branchCmd := exec.Command("git rev-parse --abbrev-ref HEAD")
+	out, err := branchCmd.Output()
+	if err != nil {
+		dlog("Error: %v\n", err)
+		return
+	}
+
+	outStr := string(out)
+	comps := strings.Split(outStr, "/")
+
+	ticketPrefix := ""
+
+	if len(comps) >= 2 {
+		ticketPrefix = fmt.Sprintf("%s ", comps[len(comps)-1])
+	}
+
+	message := strings.Join(os.Args[1:], " ")
+
+	commitCmdString := fmt.Sprintf("git commit -a \"%@%@\"", ticketPrefix, message)
+
+	fmt.Printf("%s", commitCmdString)
+
+	//commitCmd := exec.Command(commitCmdString)
+	//if err != nil {
+	//	dlog("Error: %v\n", err)
+	//	return
+	//}
+
+}
